eb5371d320c3953f37292185c326789de144c42d
Ada Chia <ada.chia.2024@computing.smu.edu.sg>
2025-11-03 17:12:01 +0800

    routing problems fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/matchup/',           
+  plugins: [vue()],          
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
