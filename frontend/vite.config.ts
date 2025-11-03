26e85b5992425207d96cbe2f66dd0f02fb4a3c2b
Ada Chia <ada.chia.2024@computing.smu.edu.sg>
2025-11-03 17:05:47 +0800

    made changes to routing since there is error during deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-//   https://vite.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/matchup/',           
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
